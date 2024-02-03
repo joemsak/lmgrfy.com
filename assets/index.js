30e428fa7f15b8c22dac21eb12680ba24279bb15
Joe Sak <joe@joesak.com>
2024-02-03 16:55:13 -0700

    Fix fix fix

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const queryPostfix = " site:reddit.com";
-  const backBtnDetector = document.getElementById("backButtonDetector");
   const qInput = document.getElementsByName("q")[0];
 
-  if (backBtnDetector.value === "1") {
-    backBtnDetector.value = 0;
-    qInput.value = qInput.value.replace(queryPostfix, "");
-  }
-
+  qInput.value = qInput.value.replace(queryPostfix, "");
   qInput.focus();
   qInput.select();
 
   document.querySelector("form").addEventListener("submit", (e) => {
-    backBtnDetector.value = 1;
     e.target.q.value = `${qInput.value}${queryPostfix}`;
   });
 });
