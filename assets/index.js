679591322a02ab3ab202d509644ec71b76e9287e
Joe Sak <joe@joesak.com>
2024-02-03 16:25:23 -0700

    UX improvrments on backbutton

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const queryPostfix = " site:reddit.com";
+  const backBtnDetector = document.getElementById("backButtonDetector");
+  const qInput = document.getElementsByName("q")[0];
+
+  qInput.focus();
+  qInput.select();
+
+  if (backBtnDetector.value === "1") {
+    backBtnDetector.value = 0;
+    console.log(qInput.value);
+    qInput.value = qInput.value.replace(queryPostfix, "");
+  }
+
   document.querySelector("form").addEventListener("submit", (e) => {
-    const formData = new FormData(e.target);
-    const formEntries = Object.fromEntries(formData.entries());
-    e.target.q.value = `${formEntries.q} site:reddit.com`;
+    backBtnDetector.value = 1;
+    e.target.q.value = `${qInput.value}${queryPostfix}`;
   });
 });
