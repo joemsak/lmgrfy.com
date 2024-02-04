e9aafa7a636b399987d50fdb26259017a4c0b41e
Joe Sak <joe@joesak.com>
2024-02-03 17:21:23 -0700

    Awesome fixes all around I rock

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const queryPostfix = " site:reddit.com";
+  const userInput = document.getElementById("userInput");
   const qInput = document.getElementsByName("q")[0];
 
-  qInput.value = qInput.value.replace(queryPostfix, "");
-  qInput.focus();
-  qInput.select();
+  qInput.value = "";
+
+  userInput.value.replace(queryPostfix, "");
+  userInput.focus();
+  userInput.select();
 
   document.querySelector("form").addEventListener("submit", (e) => {
-    e.target.q.value = `${qInput.value}${queryPostfix}`;
+    e.target.q.value = `${userInput.value}${queryPostfix}`;
   });
 });
